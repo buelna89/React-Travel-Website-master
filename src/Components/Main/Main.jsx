bb5374436840803a49e4aaf0e66aebcc28c08ebc
buelna89 <71742790+buelna89@users.noreply.github.com>
2024-06-13 15:24:09 -0700

    first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main.css'
+
+const Main = () => {
+  return (
+    <section id='main'>Main</section>
+  )
+}
+
+export default Main
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <section>Navbar</section>
+  )
+}
+
+export default Navbar
